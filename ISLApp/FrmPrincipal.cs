a39c667724cdd6033dfbd9a43527695ce48b2a4a
Fabiana Cerdas <mariacef99@gmail.com>
2022-04-27 15:06:47 -0600

    Cambios en dashboard basicos tamano minimo, se puede hacer grande la pantalla Soluionar el left bar items

---
+++ ISLApp/FrmPrincipal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace ISLApp
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUsuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+        }
+    }
+}
